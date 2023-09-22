e403e20d97b2b34a396af020c59dd5a203a8d443
nizetech <fortunepraise97@gmail.com>
2023-09-22 19:10:50 +0100

    done with decryption

--- lib/encryption/algorithms.dart
+++ lib/encryption/algorithms.dart
@@ -1,23 +1,27 @@
+import 'dart:developer';
+
 import 'package:encrypt/encrypt.dart';
 import 'package:nize_gallery/constants/constants.dart';
 
 class Algorithm {
-  static Encrypted aesEncryption(String text) {
+  String aesEncryption(String text) {
     final key = Key.fromUtf8(utf8Key);
-    final iv = IV.fromLength(16);
+    final iv = IV.allZerosOfLength(16);
 
     final encrypter = Encrypter(AES(key));
     final encrypted = encrypter.encrypt(text, iv: iv);
-    return encrypted;
+    log('enc: ' + encrypted.base64.toString());
+    return encrypted.base64;
   }
 
-  static String aesDecrytion(Encrypted text) {
+  String aesDecrytion(String encryptedText) {
     final key = Key.fromUtf8(utf8Key);
-    final iv = IV.fromLength(16);
+    final iv = IV.allZerosOfLength(16);
 
     final encrypter = Encrypter(AES(key));
-    // final encrypted = encrypter.encrypt(text, iv: iv);
-    final decrypted = encrypter.decrypt(text, iv: iv);
+    final decrypted =
+        encrypter.decrypt(Encrypted.fromBase64(encryptedText), iv: iv);
+    log('decrypted: ' + decrypted);
     return decrypted;
   }
 }
--- lib/encryption/encrytion.dart
+++ lib/encryption/encrytion.dart
@@ -1,9 +1,6 @@
 import 'dart:developer';
-
-import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
 import 'package:nize_gallery/encryption/algorithms.dart';
-// import 'package:nize_gallery/encryption/algorithms.dart';
 
 class MyEncryption extends StatefulWidget {
   const MyEncryption({super.key});
@@ -16,7 +13,15 @@ class _MyEncryptionState extends State<MyEncryption> {
   final _controller = TextEditingController();
   String encrypted = '';
   String decrypted = '';
-  late encrypt.Encrypted encrypter;
+
+  void _refresh() {
+    setState(() {
+      _controller.clear();
+      encrypted = '';
+      decrypted = '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,86 +29,90 @@ class _MyEncryptionState extends State<MyEncryption> {
         title: const Text('Encryption'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Center(
-              child: Text('AES Encryption',
-                  style: Theme.of(context).textTheme.titleLarge),
-            ),
-            SizedBox(height: 30),
-            TextField(
-              controller: _controller,
-              onChanged: (value) {
-                if (_controller.text.isEmpty)
-                  setState(() {
-                    // decrypted = 'Please enter some text';
-                    encrypted = 'Please enter some text';
-                  });
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter your text',
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _refresh();
+        },
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Center(
+                child: Text('AES Encryption',
+                    style: Theme.of(context).textTheme.titleLarge),
               ),
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              height: 48,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_controller.text.isNotEmpty) {
+              SizedBox(height: 30),
+              TextField(
+                controller: _controller,
+                onChanged: (value) {
+                  if (_controller.text.isEmpty)
                     setState(() {
-                      encrypter = Algorithm.aesEncryption(_controller.text);
-                      log(encrypter.toString());
-                      encrypted = Algorithm.aesEncryption(_controller.text)
-                          .base64
-                          .toString();
+                      encrypted = 'Please enter some text';
                     });
-                  }
                 },
-                child: const Text('Encrypt'),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your text',
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Encrypted Text: ',
-                    style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(width: 10),
-                Expanded(
-                    child: Text(encrypted,
-                        style: Theme.of(context).textTheme.titleSmall)),
-              ],
-            ),
-            SizedBox(height: 60),
-            SizedBox(
-              height: 48,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    decrypted = Algorithm.aesDecrytion(encrypter);
-                  });
-                },
-                child: const Text('Decrypt'),
+              SizedBox(height: 30),
+              SizedBox(
+                height: 48,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_controller.text.isNotEmpty) {
+                      setState(() {
+                        encrypted = Algorithm().aesEncryption(_controller.text);
+                        log(encrypted);
+                      });
+                    }
+                  },
+                  child: const Text('Encrypt'),
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Text('Decrypted Text: ',
-                    style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(width: 10),
-                Text(decrypted, style: Theme.of(context).textTheme.titleSmall),
-              ],
-            ),
-            SizedBox(height: 20),
-          ],
+              SizedBox(height: 30),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Encrypted Text: ',
+                      style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(width: 10),
+                  Expanded(
+                      child: Text(encrypted,
+                          style: Theme.of(context).textTheme.titleSmall)),
+                ],
+              ),
+              SizedBox(height: 60),
+              SizedBox(
+                height: 48,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      decrypted = Algorithm().aesDecrytion(encrypted);
+                      // _controller.text);
+                    });
+                  },
+                  child: const Text('Decrypt'),
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Text('Decrypted Text: ',
+                      style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(width: 10),
+                  Text(decrypted,
+                      style: Theme.of(context).textTheme.titleSmall),
+                ],
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
